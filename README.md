ea03f51aedc47e77094611ca7002aa4944155024
maozhongyu <crmao@qq.com>
2024-06-27 06:44:32 +0800

    对象池模式

--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 尽可能的详细的展示每一种设计模式的实现及实际使用场景
 
 
-- [单例模式](singleton/README.md) 
+- [单例模式](singleton/README.md)
+- [工厂模式](factory/README.md)
+- [对象池模式](objectpool/README.md]
+
 - [装饰器模式](decorator/README.md)
-- [函数选项模式]
-
-
+- [函数选项模式](funcoption/README.md)
 
 
--- objectpool/objectpool_test.go
+++ objectpool/objectpool_test.go
@@ -4,3 +4,23 @@
 * @Date: 2024/6/27
 **/
 package objectpool
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestObjectPool(t *testing.T) {
+	num := func() interface{} {
+		return 10.0
+	}
+	pool := NewPool(num)
+	object := pool.Acquire()
+
+	fmt.Println(pool.Inuse)
+	fmt.Println(pool.Available)
+
+	pool.Release(object)
+	fmt.Println(pool.Inuse)
+	fmt.Println(pool.Available)
+}
